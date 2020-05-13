2ab75132ccd0bc41388070c0472c5d4d3880b7da
wk1219 <sjms1215@naver.com>
2020-05-13 23:39:57 +0900

    Update network info

--- network_info.py
+++ network_info.py
@@ -1,26 +1,29 @@
 import socket
 import sys
+from requests import get
 
 host = socket.gethostname()
 mac = socket.gethostbyaddr(host)
-ip = socket.gethostbyname(host)
+other_ip = socket.gethostbyname(host)
 all = socket.getaddrinfo(host, 80)
-host_ip = socket.gethostbyname_ex(host)
+info = socket.gethostbyname_ex(host)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8",80))
-
+host_ip = s.getsockname()[0]
+ext_ip = get('https://api.ipify.org').text
 
 # print(sys.argv[0])
 #t = sys.argv[1]
 #print("http://" + t)
 
-print(type(mac))
 print("Host : %s" % host)
 print("MAC address :" + str(mac))
-print("IP address : %s" % ip)
 for i in range(0, len(all)):
     print(all[i])
-print("INFO : " + str(host_ip))
-print("IP address : %s" % s.getsockname()[0])
+print("INFO : " + str(info))
+print("IP address : %s" % host_ip)
+print("External IP : %s" % ext_ip)
+print("Other IP address : %s" % other_ip)
 url = "http://%s/Upload" % s.getsockname()[0]
 print(url)
