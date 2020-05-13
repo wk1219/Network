994e4f66f1273fb60bad3f41dca21b5e3a42ec52
wk1219 <sjms1215@naver.com>
2020-05-13 23:31:18 +0900

    Update network info

--- network_info.py
+++ network_info.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 host = socket.gethostname()
 mac = socket.gethostbyaddr(host)
@@ -8,11 +9,18 @@ host_ip = socket.gethostbyname_ex(host)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8",80))
 
-print(host)
-print(mac)
-print(ip)
-print(all)
-print(host_ip)
-print(s.getsockname()[0])
+
+# print(sys.argv[0])
+#t = sys.argv[1]
+#print("http://" + t)
+
+print(type(mac))
+print("Host : %s" % host)
+print("MAC address :" + str(mac))
+print("IP address : %s" % ip)
+for i in range(0, len(all)):
+    print(all[i])
+print("INFO : " + str(host_ip))
+print("IP address : %s" % s.getsockname()[0])
 url = "http://%s/Upload" % s.getsockname()[0]
 print(url)
