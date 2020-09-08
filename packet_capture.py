54f11524b8eaf0997a68361ea00094b6d637e5a5
wk1219 <sjms1215@naver.com>
2020-09-08 23:46:54 +0900

    Update packet_capture.py

--- packet_capture.py
+++ packet_capture.py
@@ -20,13 +20,21 @@ def sniffing(host):
     if os.name == 'nt':
         sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
 
+def packet_info():
+    print("="*20 + "INFO" + "="*20)
+    print("OS NAME : %s" % os.name)
+    print("PROTOCOL : %s" % IPPROTO_IP)
+    sniffer = socket.socket(AF_INET, SOCK_RAW, IPPROTO_IP)
+    print(sniffer)
+
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    # host = gethostbyname(gethostname())
     host = s.getsockname()[0]
     print('START SNIFF [%s]' % host)
     sniffing(host)
+    packet_info()
+
 
 if __name__=='__main__':
     main()
