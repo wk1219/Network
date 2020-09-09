ccd0d0a0301d48de46abeaf037ed8942ee5bf418
wk1219 <sjms1215@naver.com>
2020-09-09 21:34:35 +0900

    Update packet_capture.py

--- packet_capture.py
+++ packet_capture.py
@@ -1,6 +1,33 @@
 from socket import *
 import socket
 import os
+import struct
+
+def pack_ipheader(data):
+    ipheader = struct.unpack('!BBHHHBBH4s4s', data[:20])
+    return ipheader
+
+def get_protocol(ipheader):
+    protocols = {1:'ICMP', 6:'TCP', 17:'UDP'}
+    protocol = ipheader[6]
+
+    if protocol in protocols:
+        return protocols[protocol]
+    else:
+        return 'Other Protocol'
+
+def get_ip(ipheader):
+    src_ip = inet_ntoa(ipheader[8])
+    dst_ip = inet_ntoa(ipheader[9])
+    return (src_ip, dst_ip)
+
+def recv_data(sock):
+    data = ''
+    try:
+        data = sock.recvfrom(65565)
+    except timeout:
+        data = ''
+    return data[0]
 
 def sniffing(host):
     if os.name == 'nt':
@@ -9,16 +36,30 @@ def sniffing(host):
         sock_protocol = IPPROTO_ICMP
 
     sniffer = socket.socket(AF_INET, SOCK_RAW, sock_protocol)
-    sniffer.bind((host, 0))
+    sniffer.bind((host, 80))
     sniffer.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
 
     if os.name == 'nt':
         sniffer.ioctl(SIO_RCVALL, RCVALL_ON)
-    packet = sniffer.recvfrom(65565)
-    print(packet)
 
-    if os.name == 'nt':
-        sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
+    cnt = 1
+    try:
+        while True:
+            data = recv_data(sniffer)
+            ip_header = pack_ipheader(data[:20])
+            data_size = ip_header[2]
+            protocol = get_protocol(ip_header)
+            src_ip = get_ip(ip_header)[0]
+            dst_ip = get_ip(ip_header)[1]
+            print("======== SNIFFER [%d] ======== " % cnt)
+            print("Data Size : %s Bytes" % str(data_size))
+            print("Protocol : %s" % str(protocol))
+            print("Source IP : %s" % str(src_ip))
+            print("Destination IP : %s" % str(dst_ip))
+            cnt += 1
+    except KeyboardInterrupt:
+        if os.name == 'nt':
+            sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
 
 def packet_info():
     print("="*20 + "INFO" + "="*20)
@@ -33,8 +74,13 @@ def main():
     host = s.getsockname()[0]
     print('START SNIFF [%s]' % host)
     sniffing(host)
-    packet_info()
+    # while True:
+    #     if cnt == 100:
+    #         break
+    #     sniffing(host, cnt)
+    #     cnt += 1
 
+    # packet_info()
 
 if __name__=='__main__':
     main()
