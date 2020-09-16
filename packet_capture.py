81a3d75b225fd1b2807a0e00b48fe2449d180df5
wk1219 <sjms1215@naver.com>
2020-09-16 22:28:34 +0900

    Update packet_capture.py

--- packet_capture.py
+++ packet_capture.py
@@ -4,9 +4,11 @@ import os
 import struct
 import binascii
 
+
 def pack_ethernet(data):
-    eth_header = struct.unpack('!6s6s2s', data[:14])
+    eth_header = struct.unpack('!6s6sH', data[:14])
     return eth_header
+
 
 def get_mac(eth_header):
     raw_mac = binascii.hexlify(eth_header)
@@ -15,13 +17,16 @@ def get_mac(eth_header):
           (str_mac[:2], str_mac[2:4], str_mac[4:6], str_mac[6:8], str_mac[8:10], str_mac[10:12])
     return mac
 
+
 def pack_ipheader(data):
     ipheader = struct.unpack('!BBHHHBBH4s4s', data[:20])
     return ipheader
 
+
 def get_payload(data):
     payload = data[20:]
     return payload
+
 
 def get_version(ipheader):
     version = (ipheader[0] >> 4) & 0x0f
@@ -29,25 +34,32 @@ def get_version(ipheader):
         return "IPv4"
     else:
         return "IPv6"
+
+
 def get_ihl(ipheader):
     ip_header_length = ipheader[0] & 0x0f
     return ip_header_length * 4
+
 
 def get_tos(ipheader):
     tos = ipheader[1]
     return tos
 
+
 def get_id(ipheader):
     id = ipheader[3]
     return id
+
 
 def get_ttl(ipheader):
     ttl = ipheader[5]
     return ttl
 
+
 def get_protocol(ipheader):
-    protocols = {1:'ICMP', 2:'IGCMP', 6:'TCP', 9:'IGRP', 17:'UDP', 47:'GRE', 50:'ESP', 51:'AH', 57:'SKIP', 88:'EIGRP',
-                 89:'OSPF', 115:'L2TP'}
+    protocols = {1: 'ICMP', 2: 'IGCMP', 6: 'TCP', 9: 'IGRP', 17: 'UDP', 47: 'GRE', 50: 'ESP', 51: 'AH', 57: 'SKIP',
+                 88: 'EIGRP',
+                 89: 'OSPF', 115: 'L2TP'}
     protocol = ipheader[6]
 
     if protocol in protocols:
@@ -55,35 +67,43 @@ def get_protocol(ipheader):
     else:
         return 'Other Protocol'
 
+
 def get_size(ipheader):
     total_size = ipheader[2]
     return total_size
+
 
 def get_ip(ipheader):
     src_ip = inet_ntoa(ipheader[8])
     dst_ip = inet_ntoa(ipheader[9])
     return (src_ip, dst_ip)
 
+
 def pack_udpheader(data):
     udpheader = struct.unpack('!HHHH', data[:8])
     return udpheader
 
+
 def pack_tcpheader(data):
     tcpheader = struct.unpack('!HHLLBBHH2s', data[:20])
     return tcpheader
+
 
 def get_port(header):
     src_port = header[0]
     dst_port = header[1]
     return (src_port, dst_port)
 
+
 def udp_checksum(header):
     checksum = header[3]
     return checksum
 
+
 def tcp_checksum(header):
     checksum = header[7]
     return checksum
+
 
 def recv_data(sock):
     data = ''
@@ -92,6 +112,7 @@ def recv_data(sock):
     except timeout:
         data = ''
     return data[0]
+
 
 def sniffing(host):
     if os.name == 'nt':
@@ -125,15 +146,15 @@ def sniffing(host):
             if protocol == 'UDP':
                 udp_header = pack_udpheader(payload)
                 src_port, dst_port = get_port(udp_header)
-                if src_port or dst_port == 7:
+                if src_port == 7 or dst_port == 7:
                     protocol = 'Echo'
-                elif src_port or dst_port == 53:
+                elif src_port == 53 or dst_port == 53:
                     protocol = 'DNS'
-                elif src_port or dst_port == 67:
+                elif src_port == 53 or dst_port == 67:
                     protocol = 'DHCP'
-                elif src_port or dst_port == 69:
+                elif src_port == 69 or dst_port == 69:
                     protocol = 'TFTP'
-                elif src_port or dst_port == 111:
+                elif src_port == 111 or dst_port == 111:
                     protocol = 'RPC'
 
                 checksum = udp_checksum(udp_header)
@@ -141,6 +162,23 @@ def sniffing(host):
             elif protocol == 'TCP':
                 tcp_header = pack_tcpheader(payload)
                 src_port, dst_port = get_port(tcp_header)
+                if src_port == 20 or src_port == 21 or dst_port == 20 or dst_port == 21:
+                    protocol = 'FTP'
+                elif src_port == 22 or dst_port == 22:
+                    protocol = 'SSH'
+                elif src_port == 23 or dst_port == 23:
+                    protocol = 'Telnet'
+                elif src_port == 25 or dst_port == 25:
+                    protocol = 'SMTP'
+                elif src_port == 80 or dst_port == 80:
+                    protocol = 'HTTP'
+                elif src_port == 110 or dst_port == 110:
+                    protocol = 'POP3'
+                elif src_port == 143 or dst_port == 143:
+                    protocol = 'IMAP'
+                elif src_port == 443 or dst_port == 443:
+                    protocol = 'HTTPS'
+
                 checksum = tcp_checksum(tcp_header)
 
             print("======== SNIFFER [%d] ======== " % cnt)
@@ -162,20 +200,19 @@ def sniffing(host):
         if os.name == 'nt':
             sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
 
-def eth_packet_info(host):
-    print("="*20 + "INFO" + "="*20)
 
-    raw = socket(AF_INET, SOCK_RAW)
+def eth_packet_info(host):
+    print("=" * 20 + "INFO" + "=" * 20)
+
+    raw = socket(AF_INET, SOCK_RAW, IPPROTO_IP)
     raw.bind((host, 0))
     raw.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
     raw.ioctl(SIO_RCVALL, RCVALL_ON)
     data = raw.recv(65565)
 
-    eth = pack_ethernet(data[:14])
-    dst_mac = get_mac(eth[0])
-    src_mac = get_mac(eth[1])
+    eth = pack_ethernet(data)
+    dst_mac = get_mac(eth[:6])
     print(dst_mac)
-    print(src_mac)
 
 
 def main():
@@ -183,9 +220,9 @@ def main():
     s.connect(("8.8.8.8", 80))
     host = s.getsockname()[0]
     print('START SNIFF [%s]' % host)
-    # sniffing(host)
-    eth_packet_info(host)
+    sniffing(host)
+    # eth_packet_info(host)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
